813f15cae03200e6b911d02e62f97b79e2477fad
Silvano Cerza <silvanocerza@gmail.com>
2020-11-28 16:10:34 +0100

    Reworked it all

--- main.cpp
+++ main.cpp
@@ -1,75 +1,104 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
 #include <opencv2/opencv.hpp>
+#include <zbar.h>
+#include <zbar/Image.h>
+#include <zbar/Symbol.h>
 
-// helper function:
-// finds a cosine of angle between vectors
-// from pt0->pt1 and from pt0->pt2
-static double angle(cv::Point pt1, cv::Point pt2, cv::Point pt0)
+struct Barcode {
+    std::string type;
+    std::string data;
+    std::vector<cv::Point> location;
+};
+
+// Find and decode barcodes and QR codes
+static std::vector<Barcode> findBarCodes(cv::Mat& im)
 {
-    double dx1 = pt1.x - pt0.x;
-    double dy1 = pt1.y - pt0.y;
-    double dx2 = pt2.x - pt0.x;
-    double dy2 = pt2.y - pt0.y;
-    return (dx1 * dx2 + dy1 * dy2) / sqrt((dx1 * dx1 + dy1 * dy1) * (dx2 * dx2 + dy2 * dy2) + 1e-10);
+    // Create zbar scanner
+    zbar::ImageScanner scanner;
+
+    // Configure scanner
+    scanner.set_config(zbar::ZBAR_NONE, zbar::ZBAR_CFG_ENABLE, 1);
+
+    // Convert to grayscale
+    cv::Mat imGray;
+    cvtColor(im, imGray, cv::COLOR_BGR2GRAY);
+
+    // Wrap image data in a zbar image
+    zbar::Image image(im.cols, im.rows, "Y800", static_cast<uchar*>(imGray.data), im.cols * im.rows);
+
+    // Scan the image for barcodes and QRCodes
+    int n = scanner.scan(image);
+    if (n == 0) {
+        return {};
+    }
+
+    std::vector<Barcode> objects;
+    // Gather results
+    auto it = image.symbol_begin();
+    while (it != image.symbol_end()) {
+        Barcode obj;
+
+        obj.type = it->get_type_name();
+        obj.data = it->get_data();
+
+        // Obtain location
+        for (int i = 0; i < it->get_location_size(); i++) {
+            obj.location.push_back(cv::Point(it->get_location_x(i), it->get_location_y(i)));
+        }
+
+        objects.push_back(obj);
+        ++it;
+    }
+    return objects;
+}
+
+// Display barcode and QR code location
+static void display(cv::Mat& im, std::vector<Barcode>& codes)
+{
+    // Loop over all decoded objects
+    for (auto code : codes) {
+        std::vector<cv::Point> points = code.location;
+        std::vector<cv::Point> hull;
+
+        // If the points do not form a quad, find convex hull
+        if (points.size() > 4) {
+            cv::convexHull(points, hull);
+        } else {
+            hull = points;
+        }
+
+        // Number of points in the convex hull
+        int n = hull.size();
+
+        for (int j = 0; j < n; j++) {
+            cv::line(im, hull[j], hull[(j + 1) % n], cv::Scalar(255, 0, 0), 3);
+        }
+        std::cout << code.data << std::endl;
+    }
+
+    // Display results
+    cv::imshow("Results", im);
 }
 
 int main(int argc, char** argv)
 {
-    if (argc != 2) {
-        std::cout << "Missing image file argument" << std::endl;
-        return -1;
-    }
-
-    cv::Mat original = cv::imread(argv[1]);
+    cv::VideoCapture cam;
+    // Gets the first cam it finds
+    cam.open(0, cv::CAP_ANY);
     cv::Mat image;
+    while (cam.isOpened()) {
+        cam.read(image);
+        auto codes = findBarCodes(image);
 
-    // Converts in memory loaded image from BGR to HSV
-    cv::cvtColor(original, image, cv::COLOR_BGR2HSV);
+        display(image, codes);
 
-    auto lowerBound = cv::Scalar(0, 0, 0);
-    auto upperBound = cv::Scalar(50, 255, 150);
-
-    cv::inRange(image, lowerBound, upperBound, image);
-
-    std::vector<std::vector<cv::Point>> contours;
-    std::vector<cv::Vec4i> hierarchy;
-    cv::findContours(image, contours, hierarchy, cv::RETR_LIST, cv::CHAIN_APPROX_NONE);
-    //    cv::findContours(image, contours, cv::RETR_LIST, cv::CHAIN_APPROX_TC89_L1);
-
-    //    for (int i = 0; i >= 0; i = hierarchy[i][0]) {
-    //        cv::drawContours(original, contours, i, cv::Scalar(0, 0, 255), cv::LINE_4, 1, hierarchy);
-    //    }
-
-    //    std::vector<cv::Point> approx;
-    //    std::vector<std::vector<cv::Point>> squares;
-    //    for (int i = 0; i < contours.size(); i++) {
-    //        cv::approxPolyDP(contours[i], approx, cv::arcLength(contours[i], true) * 0.02, true);
-    //        if (/*approx.size() == 4 && */ std::fabs(cv::contourArea(approx) > 1000) && cv::isContourConvex(approx)) {
-    //            double maxCosine = 0;
-    //            //            for (int j = 2; j < 5; j++) {
-    //            //                double cosine = std::fabs(angle(approx[j % 4], approx[j - 2], approx[j - 1]));
-    //            //                maxCosine = std::max(maxCosine, cosine);
-    //            //            }
-
-    //            if (maxCosine < 0.4) {
-    //                squares.push_back(approx);
-    //            }
-    //        }
-    //    }
-
-    //    for (int i = 0; i < squares.size(); i++) {
-    //        cv::polylines(original, squares[i], true, cv::Scalar(0, 0, 255), 3, cv::LINE_AA);
-    //    }
-
-    cv::resize(image, image, cv::Size(image.cols, image.rows) / 3);
-    cv::imshow("Converted", image);
-
-    cv::resize(original, original, cv::Size(original.cols, original.rows) / 3);
-    cv::imshow("Original", original);
-
-    cv::waitKey(0);
+        if (cv::waitKey(5) > 0) {
+            break;
+        }
+    }
 
     return 0;
 }
